15f4dd5cc29899484f66afb511d7018d81cdf717
nicolas asparria <nasparria@gmail.com>
2022-06-17 18:33:05 -0500

    Feature 5 complete

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView
 
+from projects.models import Project
 # Create your views here.
+
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = "project_list.html"
+    context_object_name = "projects_list"
