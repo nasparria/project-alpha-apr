a588044b77639c0267f93a034dd62a32e76210d9
nicolas asparria <nasparria@gmail.com>
2022-06-20 16:46:46 -0500

    Feature 10 complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from accounts.views import signup
 
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("signup/", signup, name="signup"),
 ]
